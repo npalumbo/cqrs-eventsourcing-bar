66a77e7aefd4cfdc15455df901e7345dfc76e5d3
Nico <napalumbo@gmail.com>
2024-11-25 00:28:29 +0100

    Add MarkDrinksServed command implementation

--- aggregates/tab_aggregate.go
+++ aggregates/tab_aggregate.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"golangsevillabar/commands"
+	"golangsevillabar/domain"
 	"golangsevillabar/events"
+	"golangsevillabar/utils"
 )
 
 type CommandHandler[T commands.Command] interface {
@@ -15,6 +17,7 @@ type EventApplier[E events.Event] interface {
 	apply(e E) error
 }
 
+// Command Handlers
 type openTabHandler struct {
 	eventEmitter events.EventEmitter[events.Event]
 }
@@ -22,15 +25,33 @@ type placeOrderHandler struct {
 	tabAggregate *TabAggregate
 	eventEmitter events.EventEmitter[events.Event]
 }
+type markDrinksServedHandler struct {
+	tabAggregate *TabAggregate
+	eventEmitter events.EventEmitter[events.Event]
+}
+
+// Event Appliers
 type tabOpenedApplier struct {
+	tabAggregate *TabAggregate
+}
+type drinksOrderedApplier struct {
+	tabAggregate *TabAggregate
+}
+type drinksServedApplier struct {
 	tabAggregate *TabAggregate
 }
 
 type TabAggregate struct {
 	tabOpen           bool
-	openTabHandler    CommandHandler[commands.OpenTab]
-	placeOrderHandler CommandHandler[commands.PlaceOrder]
-	TabOpenedHandler  EventApplier[events.TabOpened]
+	outstandingDrinks []domain.OrderedItem
+	// Command Handlers
+	openTabHandler          CommandHandler[commands.OpenTab]
+	placeOrderHandler       CommandHandler[commands.PlaceOrder]
+	markDrinksServedHandler CommandHandler[commands.MarkDrinksServed]
+	// Event Appliers
+	tabOpenedApplier     EventApplier[events.TabOpened]
+	drinksOrderedApplier EventApplier[events.DrinksOrdered]
+	drinksServedApplier  EventApplier[events.DrinkServed]
 }
 
 func (t TabAggregate) HandleCommand(c commands.Command) error {
@@ -39,6 +60,8 @@ func (t TabAggregate) HandleCommand(c commands.Command) error {
 		return t.openTabHandler.handle(command)
 	case commands.PlaceOrder:
 		return t.placeOrderHandler.handle(command)
+	case commands.MarkDrinksServed:
+		return t.markDrinksServedHandler.handle(command)
 	default:
 		return fmt.Errorf("unexpected commands.Command: %#v", c)
 	}
@@ -47,7 +70,11 @@ func (t TabAggregate) HandleCommand(c commands.Command) error {
 func (t TabAggregate) ApplyEvent(e events.Event) error {
 	switch event := e.(type) {
 	case events.TabOpened:
-		return t.TabOpenedHandler.apply(event)
+		return t.tabOpenedApplier.apply(event)
+	case events.DrinksOrdered:
+		return t.drinksOrderedApplier.apply(event)
+	case events.DrinkServed:
+		return t.drinksServedApplier.apply(event)
 	default:
 		return fmt.Errorf("unexpected events.Event: %#v", e)
 	}
@@ -66,8 +93,28 @@ func (p placeOrderHandler) handle(c commands.PlaceOrder) error {
 	return errors.New("tab is not opened")
 }
 
+func (m markDrinksServedHandler) handle(c commands.MarkDrinksServed) error {
+	menuItemsThatAreNotInOrderedItems := utils.FindMenuItemsThatAreNotInOrderedItems(m.tabAggregate.outstandingDrinks, c.MenuNumbers)
+	if len(menuItemsThatAreNotInOrderedItems) > 0 {
+		return fmt.Errorf("cannot serve drinks that were not ordered: %v", menuItemsThatAreNotInOrderedItems)
+	}
+
+	m.eventEmitter.EmitEvent(events.DrinkServed{ID: c.ID, MenuNumbers: c.MenuNumbers})
+	return nil
+}
+
 func (t tabOpenedApplier) apply(e events.TabOpened) error {
 	t.tabAggregate.tabOpen = true
+	return nil
+}
+
+func (d drinksOrderedApplier) apply(e events.DrinksOrdered) error {
+	d.tabAggregate.outstandingDrinks = e.Items
+	return nil
+}
+
+func (d drinksServedApplier) apply(e events.DrinkServed) error {
+	d.tabAggregate.outstandingDrinks = utils.RemoveOrderedItemsThatAppearInMarkedServedItems(d.tabAggregate.outstandingDrinks, e.MenuNumbers)
 	return nil
 }
 
@@ -81,7 +128,17 @@ func CreateTabAggregate(eventEmitter events.EventEmitter[events.Event]) (tabAggr
 			tabAggregate: &tabAggregate,
 			eventEmitter: eventEmitter,
 		},
-		TabOpenedHandler: tabOpenedApplier{
+		markDrinksServedHandler: markDrinksServedHandler{
+			tabAggregate: &tabAggregate,
+			eventEmitter: eventEmitter,
+		},
+		tabOpenedApplier: tabOpenedApplier{
+			tabAggregate: &tabAggregate,
+		},
+		drinksOrderedApplier: drinksOrderedApplier{
+			tabAggregate: &tabAggregate,
+		},
+		drinksServedApplier: drinksServedApplier{
 			tabAggregate: &tabAggregate,
 		},
 	}
--- aggregates/tab_aggregate_test.go
+++ aggregates/tab_aggregate_test.go
@@ -62,7 +62,7 @@ func (suite *TabAggregateTestSuite) TestCanNotOrderWithUnOpenedTab() {
 	// When
 	err := suite.tabAggregate.HandleCommand(commands.PlaceOrder{
 		ID:    commandID,
-		Items: []domain.OrderedItem{{MenuItem: 11, Description: "cruzcampo", Price: 1.5}},
+		Items: []domain.OrderedItem{{MenuItem: 11, Description: "beer", Price: 1.5}},
 	})
 
 	// Then
@@ -84,7 +84,7 @@ func (suite *TabAggregateTestSuite) TestCanOrderWhenTabIsOpen() {
 	// When
 	err := suite.tabAggregate.HandleCommand(commands.PlaceOrder{
 		ID:    placeOrderCommandID,
-		Items: []domain.OrderedItem{{MenuItem: 11, Description: "cruzcampo", Price: 1.5}},
+		Items: []domain.OrderedItem{{MenuItem: 11, Description: "beer", Price: 1.5}},
 	})
 
 	// Then
@@ -92,8 +92,87 @@ func (suite *TabAggregateTestSuite) TestCanOrderWhenTabIsOpen() {
 	suite.eventEmitter.AssertNumberOfCalls(t, "EmitEvent", 1)
 	suite.eventEmitter.AssertCalled(t, "EmitEvent", events.DrinksOrdered{
 		ID:    placeOrderCommandID,
-		Items: []domain.OrderedItem{{MenuItem: 11, Description: "cruzcampo", Price: 1.5}},
+		Items: []domain.OrderedItem{{MenuItem: 11, Description: "beer", Price: 1.5}},
 	})
+}
+
+func (suite *TabAggregateTestSuite) TestOrderedDrinksCanBeServed() {
+	tabOpenedEventID, _ := ksuid.NewRandom()
+	drinksOrderedEventID, _ := ksuid.NewRandom()
+	markDrinksServedID, _ := ksuid.NewRandom()
+	t := suite.T()
+
+	// Given
+	_ = suite.tabAggregate.ApplyEvent(events.TabOpened{ID: tabOpenedEventID, Waiter: "waiter_1", TableNumber: 0})
+	_ = suite.tabAggregate.ApplyEvent(events.DrinksOrdered{ID: drinksOrderedEventID, Items: []domain.OrderedItem{
+		{MenuItem: 11, Description: "beer", Price: 1.5},
+		{MenuItem: 12, Description: "water", Price: 1.0},
+	}})
+
+	// When
+	err := suite.tabAggregate.HandleCommand(commands.MarkDrinksServed{
+		ID:          markDrinksServedID,
+		MenuNumbers: []int{11, 12},
+	})
+
+	// Then
+	assert.NoError(t, err)
+	suite.eventEmitter.AssertNumberOfCalls(t, "EmitEvent", 1)
+	suite.eventEmitter.AssertCalled(t, "EmitEvent", events.DrinkServed{
+		ID:          markDrinksServedID,
+		MenuNumbers: []int{11, 12},
+	})
+}
+
+func (suite *TabAggregateTestSuite) TestCannotServeUnorderedDrinks() {
+	tabOpenedEventID, _ := ksuid.NewRandom()
+	drinksOrderedEventID, _ := ksuid.NewRandom()
+	markDrinksServedID, _ := ksuid.NewRandom()
+	t := suite.T()
+
+	// Given
+	_ = suite.tabAggregate.ApplyEvent(events.TabOpened{ID: tabOpenedEventID, Waiter: "waiter_1", TableNumber: 0})
+	_ = suite.tabAggregate.ApplyEvent(events.DrinksOrdered{ID: drinksOrderedEventID, Items: []domain.OrderedItem{
+		{MenuItem: 11, Description: "beer", Price: 1.5},
+		{MenuItem: 12, Description: "water", Price: 1.0},
+	}})
+
+	// When
+	err := suite.tabAggregate.HandleCommand(commands.MarkDrinksServed{
+		ID:          markDrinksServedID,
+		MenuNumbers: []int{11, 13},
+	})
+
+	// Then
+	assert.Error(t, err)
+	assert.Equal(t, "cannot serve drinks that were not ordered: [13]", err.Error())
+	suite.eventEmitter.AssertNumberOfCalls(t, "EmitEvent", 0)
+}
+
+func (suite *TabAggregateTestSuite) TestCannotServeTheSameOrderedDrinkTwice() {
+	tabOpenedEventID, _ := ksuid.NewRandom()
+	drinksOrderedEventID, _ := ksuid.NewRandom()
+	drinksServedID, _ := ksuid.NewRandom()
+	markDrinksServedID, _ := ksuid.NewRandom()
+	t := suite.T()
+
+	// Given
+	_ = suite.tabAggregate.ApplyEvent(events.TabOpened{ID: tabOpenedEventID, Waiter: "waiter_1", TableNumber: 0})
+	_ = suite.tabAggregate.ApplyEvent(events.DrinksOrdered{ID: drinksOrderedEventID, Items: []domain.OrderedItem{
+		{MenuItem: 11, Description: "beer", Price: 1.5},
+	}})
+	_ = suite.tabAggregate.ApplyEvent(events.DrinkServed{ID: drinksServedID, MenuNumbers: []int{11}})
+
+	// When
+	err := suite.tabAggregate.HandleCommand(commands.MarkDrinksServed{
+		ID:          markDrinksServedID,
+		MenuNumbers: []int{11},
+	})
+
+	// Then
+	assert.Error(t, err)
+	assert.Equal(t, "cannot serve drinks that were not ordered: [11]", err.Error())
+	suite.eventEmitter.AssertNumberOfCalls(t, "EmitEvent", 0)
 }
 
 func TestSuite(t *testing.T) {
